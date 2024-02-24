9d34a01b0693239cbe17e411c5b963e0c4be113b
Gabriel Almeida <1gabrielsantosdealmeida@gmail.com>
2024-02-24 15:34:35 -0300

    Implementing content negotiation to return JSON & XML

--- curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
 
 @RestController
-@RequestMapping("/cidades")
+@RequestMapping(value="/cidades", produces= MediaType.APPLICATION_JSON_VALUE)
 public class CidadeController {
 
 	@Autowired
