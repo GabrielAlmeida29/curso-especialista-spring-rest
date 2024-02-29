7d8521e987761579071b6e75fc0ef91a7850a08b
Gabriel Almeida <1gabrielsantosdealmeida@gmail.com>
2024-02-28 21:55:49 -0300

    4.26 Modeling and Implementing DELETE resource with DELETE

--- curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -6,6 +6,7 @@ import com.algaworks.algafood.api.model.CozinhasXmlWapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -62,6 +63,23 @@ public class CozinhaController {
 
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+		try {
+			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+			if (cozinha != null) {
+				cozinhaRepository.remover(cozinha);
+
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+		} catch (DataIntegrityViolationException e){
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
 
