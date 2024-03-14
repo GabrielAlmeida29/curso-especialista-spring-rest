e397e2105de1f0bf5a1a40bd27fc6df791a795c2
Gabriel Almeida <1gabrielsantosdealmeida@gmail.com>
2024-03-14 19:03:11 -0300

    5.6 Creating queries with query methods

--- curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-	//List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findByNome(String nome);
+
+    Optional<Cozinha> findOneCozinhaByNome(String nome);
 
 }
