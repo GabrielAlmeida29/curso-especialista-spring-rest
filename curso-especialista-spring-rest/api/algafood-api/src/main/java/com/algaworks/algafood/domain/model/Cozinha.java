e98725ab9ddc2bcd02a2e3b11fd4bc7f597bea95
Gabriel Almeida <1gabrielsantosdealmeida@gmail.com>
2024-04-15 20:07:55 -0300

    6.12 Modifying the fetching strategy to Lazy Load

--- curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ curso-especialista-spring-rest/api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,13 +1,11 @@
 package com.algaworks.algafood.domain.model;
 
-import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
